8206a5349f01d393a574ee1500f10307818e06ff
Giorgio22 <giorgioprizzi@gmail.com>
2023-06-08 17:34:07 +0200

    FIne

---
+++ resources/views/partials/bluesection.blade.php
@@ -0,0 +1,35 @@
+<div class="blue-bar">
+    <ul class="container">
+      <li>
+        <div class="img">
+          <img src="../src/img/buy-comics-digital-comics.png" alt="">
+        </div>
+        <a href="">DIGITAL COMICS</a>
+      </li>
+      <li>
+        <div class="img">
+          <img src="../src/img/buy-comics-merchandise.png" alt="">
+        </div>
+        <a href="">DC MERCHANDISE</a>
+      </li>
+      <li>
+        <div class="img">
+          <img src="../src/img/buy-comics-subscriptions.png" alt="">
+        </div>
+        <a href="">SUSCRIPTION</a>
+      </li>
+      <li>
+        <div class="img">
+          <img src="../src/img/buy-comics-shop-locator.png" alt="">
+        </div>
+        <a href="">COMIC SHOP LOCATOR</a>
+      </li>
+      <li>
+        <div class="img">
+          <img src="../src/img/buy-dc-power-visa.svg" alt="">
+        </div>
+        <a href="">DC POWER VISA</a>
+      </li>
+
+    </ul>
+</div>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,65 @@
+<header>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <div class="banner-blue text-white text-end">
+                <div class="container">
+                    <span>DC POWER VISA &REG;</span>
+                    <span class="dropdown">
+                        <span class="dropdown-toggle ms-4" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            ADDITIONAL DC SITES
+                        </span>
+                    </span>
+                </div>
+            </div>
+
+
+
+            <a class="navbar-brand" href="#">
+                <img src="../src/img/dc-logo.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav me-5">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CHARACTERS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">COMICS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">MOVIES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">TV</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">GAMES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">COLLECTIBLES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">VIDEOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">FANS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">NEWS</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            SHOP
+                        </a>
+                    </li>
+                </ul>
+                <form action="" method="">
+                    <input type="text" placeholder="Search" class="text-end">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </form>
+            </div>
+        </div>
+    </nav>
+</header>
